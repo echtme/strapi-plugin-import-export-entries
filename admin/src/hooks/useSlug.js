6439df70515c7b3260ba89d3b30c9d52a9d3cb8c
Denis <deniskrueger@gmail.com>
2024-05-24 16:47:46 +0400

    Update useSlug.js

--- admin/src/hooks/useSlug.js
+++ admin/src/hooks/useSlug.js
@@ -7,7 +7,8 @@ export const useSlug = () => {
   const { pathname } = useLocation();
 
   const slug = useMemo(() => {
-    const matches = pathname.match(/content-manager\/(collectionType|singleType)\/([a-zA-Z0-9\-:_.]*)/);
+    // const matches = pathname.match(/content-manager\/(collectionType|singleType)\/([a-zA-Z0-9\-:_.]*)/);
+    const matches = pathname.match(/content-manager\/(collection-types|single-types)\/([a-zA-Z0-9\-:_.]*)/);
     return matches?.[2] ? matches[2] : SLUG_WHOLE_DB;
   }, [pathname]);
 
