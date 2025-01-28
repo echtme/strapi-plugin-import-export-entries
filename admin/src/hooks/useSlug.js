3ccfb47952dcbb1c8ac216cf5194bb22976587f5
Denis <deniskrueger@gmail.com>
2025-01-28 17:20:36 +0400

    Update useSlug.js

--- admin/src/hooks/useSlug.js
+++ admin/src/hooks/useSlug.js
@@ -7,7 +7,10 @@ export const useSlug = () => {
   const { pathname } = useLocation();
 
   const slug = useMemo(() => {
-    const matches = pathname.match(/content-manager\/(collectionType|singleType)\/([a-zA-Z0-9\-:_.]*)/);
+    // const matches = pathname.match(/content-manager\/(collectionType|singleType)\/([a-zA-Z0-9\-:_.]*)/);
+    // fix for Strapi v4.17.1
+    const matches = pathname.match(/content-manager\/(collection-types|single-types)\/([a-zA-Z0-9\-:_.]*)/);
+
     return matches?.[2] ? matches[2] : SLUG_WHOLE_DB;
   }, [pathname]);
 
